41cba9f9c6d9653d1bc6b1e1e04c03e2569e5ed3
kpfister <kpfister@me.com>
2018-03-01 19:39:50 -0700

    Starting Commit
    The first commit on a new challenge!

---
+++ Wag-OBJ-C/Wag-CC-OBJ-C/Wag-CC-OBJ-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wag-CC-OBJ-C
+//
+//  Created by Karl Pfister on 3/1/18.
+//  Copyright © 2018 Karl Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
